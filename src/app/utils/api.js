94adc4cd71a7e51756d235d73e792ea23f3dc399
saadahmad2901 <saadahmad2901@gmail.com>
2026-02-04 00:45:21 +0500

    test

--- src/app/utils/api.js
+++ src/app/utils/api.js
@@ -3,10 +3,10 @@ import NProgress from "nprogress";
 import "nprogress/nprogress.css";
 
 let activeRequests = 0;
+let progress = 0;
 
 const API = axios.create({
-//   baseURL: process.env.NEXT_PUBLIC_API,
-baseURL:"https://bookvenu.up.railway.app/api/v1/",
+  baseURL: "https://bookvenu.up.railway.app/api/v1/",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
@@ -15,33 +15,46 @@ baseURL:"https://bookvenu.up.railway.app/api/v1/",
 });
 
 /* ---------- REQUEST ---------- */
-API.interceptors.request.use(
-  (config) => {
-    if (activeRequests === 0) {
-      NProgress.start();
+API.interceptors.request.use((config) => {
+  if (activeRequests === 0) {
+    NProgress.start();
+
+    // Add percent text element if not exists
+    if (!document.querySelector(".spinner-percent")) {
+      const percentText = document.createElement("span");
+      percentText.className = "spinner-percent";
+      percentText.innerText = "0%";
+      document.querySelector(".spinner").appendChild(percentText);
     }
-    activeRequests++;
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
+
+    progress = 0;
+    const interval = setInterval(() => {
+      if (progress < 95) {
+        progress += Math.random() * 5; // simulate progress
+        document.querySelector(".spinner-percent").innerText =
+          Math.floor(progress) + "%";
+      } else {
+        clearInterval(interval);
+      }
+    }, 200);
   }
-);
+  activeRequests++;
+  return config;
+});
 
 /* ---------- RESPONSE ---------- */
 API.interceptors.response.use(
   (response) => {
     activeRequests--;
     if (activeRequests === 0) {
-      NProgress.done();
+      document.querySelector(".spinner-percent").innerText = "100%";
+      setTimeout(() => NProgress.done(), 200); // small delay to show 100%
     }
     return response;
   },
   (error) => {
     activeRequests--;
-    if (activeRequests === 0) {
-      NProgress.done();
-    }
+    if (activeRequests === 0) NProgress.done();
     return Promise.reject(error);
   }
 );
